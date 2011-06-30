5e612c0514c8a8b3380231368c35ecf30dfff92e
Adam Miribyan <adam.miribyan@gmail.com>
2011-06-30 22:00:25 +0200

    gig validation

--- test/unit/gig_test.rb
+++ test/unit/gig_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class GigTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validates presence of all fields" do
+    placebo_gig = FactoryGirl.build(:gig)
+    madonna_gig = FactoryGirl.build(:gig, name: nil)
+    
+    assert placebo_gig.valid?
+    assert madonna_gig.invalid?
+  end
 end
