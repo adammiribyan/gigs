d4005fcb1397494b70296b41f28f7ebd59b09c56
Adam Miribyan <adam.miribyan@gmail.com>
2011-07-01 11:56:59 +0200

    gig show footnotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
+# encoding: utf-8
+
 module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
+  end
+  
+  def link_to_home
+    unless controller.action_name == "index" and controller.controller_name == "gigs"
+      "<div id=\"home\">#{link_to('↑↑↑', root_path)}</div>".html_safe
+    end
   end
 end
