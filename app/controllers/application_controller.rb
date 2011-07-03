ede4822bf16c1892d534aa9d72a63974d3ee2cbb
Adam Miribyan <adam.miribyan@gmail.com>
2011-07-03 17:52:16 +0200

    turned off http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # HTTP Authentication
-  http_basic_authenticate_with name: "admin", password: "monkey"
+  # http_basic_authenticate_with name: "admin", password: "monkey"
   
   protect_from_forgery
 end
